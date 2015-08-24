b6848ad24c91669d38128be5a7c2ce2d930f8f5a
Peter Wong <peterwcm@hotmail.com>
2015-08-24 23:56:20 +1000

    style updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,17 @@
 @section('content')
 
 <div class="home-banner">
-    <div style="color:white;text-align:center;padding-top:180px;font-size:45px;letter-spacing: 2px;font-weight:700;">
-        DISCOVER &amp; SHARE
-    </div>
-    <div style="color:white;text-align:center;padding-top:15px;font-size:20px;letter-spacing: 1px;">
-        Experience the most amazing local adventures in your city.
-    </div>
+    <div class="banner-h1">DISCOVER &amp; SHARE</div>
+    <div class="banner-h2">Experience the most amazing local adventures in your city.</div>
 
-    <a href="/search">
+    <a href="{{ url() }}">
         <div id="start-now">
             <span>Get Started</span>
             <span class="fa fa-chevron-right" style="margin-left:5px;font-size:12px !important;"></span>
         </div>
     </a>
 
-    <video width="100%" height="100%" loop="" poster="https://d3ce7jb7ih53ua.cloudfront.net/verloca_new_design/main_page/first-page-1.png" id="bgvid" style="min-width:100%;" autoplay="">
+    <video width="100%" height="100%" loop="loop" autoplay="autoplay" poster="https://d3ce7jb7ih53ua.cloudfront.net/verloca_new_design/main_page/first-page-1.png">
         <source src="https://d3ce7jb7ih53ua.cloudfront.net/verloca_new_design/main_page/verlocal+header_3.mp4" type="video/mp4">
         <source src="https://d3ce7jb7ih53ua.cloudfront.net/verloca_new_design/main_page/verlocal+header_1.webm" type="video/webm">
     </video>
