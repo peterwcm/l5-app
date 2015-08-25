2c31cbce016e947bd75c49fcac5a4b63e885ab39
Peter Wong <peterwcm@hotmail.com>
2015-08-25 23:19:33 +1000

    added new section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,47 +23,35 @@
     </div>
 
     <div class="home-content">
-        <div class="featured-contents">
+        <div class="featured-info">
             <div class="featured-title">What's New</div>
-            <div class="whatsnew-content">
+            <div class="featured-content">
                 <a href="{{ url() }}">
                     <div class="inline deal-box">
-                        <div style="background-image: url(https://www.filepicker.io/api/file/3RC1MNURpWl68t1MDmNj);
-                            background-size: cover;background-position: center;width:300px;height:150px;">
+                        <div class="featured-image" style="background-image: url(https://www.filepicker.io/api/file/3RC1MNURpWl68t1MDmNj);"></div>
+                        <div class="featured-icon">
+                            <span class="fa fa-chevron-right"></span>
                         </div>
-                        <div style="margin:0 auto;margin-top:-20px;width:40px;height:40px;border-radius:40px;
-                            background:#88c5cc;">
-                            <span class="fa fa-chevron-right" style="color:white;line-height:45px;margin-left:2px;
-                            font-weight:300 !important;font-size:12px !important;"></span>
-                        </div>
-                        <div style="margin-top:12px;font-size:14px;padding-left:20px;padding-right:20px;color:#999999;">
-                            Become a Japanese Chef - Homemade Ramen and Gyoza!</div></div>
+                        <div class="featured-text">Become a Japanese Chef - Homemade Ramen and Gyoza!</div>
+                    </div>
                 </a>
-                <a href="/event/nyc-crafting-painting-silkscarf">
+                <a href="{{ url() }}">
                     <div class="inline deal-box">
-                        <div style="background-image: url(https://www.filepicker.io/api/file/3RC1MNURpWl68t1MDmNj);
-                            background-size: cover;background-position: center;width:300px;height:150px;">
+                        <div class="featured-image" style="background-image: url(https://www.filepicker.io/api/file/9MjXpLJRrCIdTb9tSHCQ);"></div>
+                        <div class="featured-icon">
+                            <span class="fa fa-chevron-right"></span>
                         </div>
-                        <div style="margin:0 auto;margin-top:-20px;width:40px;height:40px;border-radius:40px;
-                            background:#88c5cc;">
-                            <span class="fa fa-chevron-right" style="color:white;line-height:45px;margin-left:2px;
-                            font-weight:300 !important;font-size:12px !important;"></span>
-                        </div>
-                        <div style="margin-top:12px;font-size:14px;padding-left:20px;padding-right:20px;color:#999999;">
-                            Become a Japanese Chef - Homemade Ramen and Gyoza!</div></div>
+                        <div class="featured-text">Silk Scarf Painting Workshop with a True Artisan!</div>
+                    </div>
                 </a>
-                <a href="/event/nyc-crafting-painting-silkscarf">
+                <a href="{{ url() }}">
                     <div class="inline deal-box">
-                        <div style="background-image: url(https://www.filepicker.io/api/file/3RC1MNURpWl68t1MDmNj);
-                            background-size: cover;background-position: center;width:300px;height:150px;">
+                        <div class="featured-image" style="background-image: url(https://www.filepicker.io/api/file/WMeFGMbuSRaoQHAt1grP);"></div>
+                        <div class="featured-icon">
+                            <span class="fa fa-chevron-right"></span>
                         </div>
-                        <div style="margin:0 auto;margin-top:-20px;width:40px;height:40px;border-radius:40px;
-                            background:#88c5cc;">
-                            <span class="fa fa-chevron-right" style="color:white;line-height:45px;margin-left:2px;
-                            font-weight:300 !important;font-size:12px !important;"></span>
-                        </div>
-                        <div style="margin-top:12px;font-size:14px;padding-left:20px;padding-right:20px;color:#999999;">
-                            Become a Japanese Chef - Homemade Ramen and Gyoza!</div></div>
+                        <div class="featured-text">SoCal Overnight Surfing Trip (Camping Included)</div>
+                    </div>
                 </a>
             </div>
         </div>
