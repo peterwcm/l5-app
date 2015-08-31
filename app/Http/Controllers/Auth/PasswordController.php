224769eaf1e7ec355f27b116472d12252e5dfa0d
Peter Wong <peterwcm@hotmail.com>
2015-08-31 22:09:11 +1000

    5.1.0 update prep

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -1,8 +1,6 @@
 <?php namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Contracts\Auth\PasswordBroker;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller {
@@ -20,18 +18,13 @@ class PasswordController extends Controller {
 
 	use ResetsPasswords;
 
-	/**
-	 * Create a new password controller instance.
-	 *
-	 * @param \Illuminate\Contracts\Auth\Guard $auth
-	 * @param \Illuminate\Contracts\Auth\PasswordBroker $passwords
-	 */
-	public function __construct(Guard $auth, PasswordBroker $passwords)
-	{
-		$this->auth = $auth;
-		$this->passwords = $passwords;
-
-		$this->middleware('guest');
-	}
-
+    /**
+     * Create a new password controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
 }
