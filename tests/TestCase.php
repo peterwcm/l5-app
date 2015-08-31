839a26bc2cb2cbd29b1d5a86934812b5dfb6896c
Peter Wong <peterwcm@hotmail.com>
2015-08-31 22:55:17 +1000

    more 5.1 updates

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,7 +1,7 @@
 <?php
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase {
-
+class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
     /**
      * The base URL to use while testing the application.
      *
@@ -9,18 +9,17 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
      */
     protected $baseUrl = 'http://localhost';
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
 
-		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-		return $app;
-	}
-
+        return $app;
+    }
 }
