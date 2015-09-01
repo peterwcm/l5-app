931a8e08442449091cbf1338bd41fb6de7e36515
Peter Wong <peterwcm@hotmail.com>
2015-09-01 22:59:13 +1000

    menu fix

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -11,59 +11,7 @@
             <!-- Right Nav Section -->
             <ul class="right">
                 @if(Auth::check())
-                    <span class="mobile-appear">
-                        <li>
-                            <a href="{{ URL::route('messages') }}">
-                                &nbsp;<span class='fa fa-envelope fa-lg'></span>
-                                <span>&nbsp;&nbsp;&nbsp;Messages</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ URL::route('profile.edit') }}">
-                                &nbsp;<span class='fa fa-user fa-lg'></span>
-                                <span>&nbsp;&nbsp;&nbsp;&nbsp;Profile</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ URL::route('bookings') }}">
-                                &nbsp;<span class='fa fa-calendar fa-lg'></span>
-                                <span>&nbsp;&nbsp;&nbsp;Bookings</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ URL::route('lessons.new') }}">
-                                &nbsp;<span class='fa fa-graduation-cap fa-lg'></span>
-                                <span>&nbsp;&nbsp;&nbsp;Teach</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ URL::route('settings') }}">
-                                &nbsp;<span class='fa fa-cog fa-lg'></span>
-                                <span>&nbsp;&nbsp;&nbsp;&nbsp;Account</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ URL::route('logout') }}">
-                                &nbsp;<span class='fa fa-sign-out fa-lg'></span>
-                                <span>&nbsp;&nbsp;&nbsp;&nbsp;Logout</span>
-                            </a>
-                        </li>
-                    </span>
-                    <li class="mobile-disappear menu-highlight active">{{ link_to_route('lessons.new', 'Teach') }}</li>
-                    <li class="mobile-disappear">
-                        <a href="{{ URL::route('messages') }}">
-                            <span class='fa fa-envelope fa-lg'></span>
-                        </a>
-                    </li>
-                    <li class="mobile-disappear has-dropdown">
-                        <a href="#"><img class="menu-profile-picture" src='{{ url('images/lav_pic.jpg') }}'> {{{ Auth::user()->first_name }}}</a>
-                        <ul class="dropdown">
-                            {{--<li>{{ route('profile.edit', 'Profile') }}</li>--}}
-                            {{--<li>{{ route('bookings', 'Bookings') }}</li>--}}
-                            {{--<li>{{ route('settings', 'Account') }}</li>--}}
-                            {{--<li>{{ route('logout', 'Logout') }}</li>--}}
-                        </ul>
-                    </li>
+                    <li><a href="{{ url() }}">Logged In!</a></li>
                 @else
                     {{--<li class="active">{{ route('lessons.new', 'Teach') }}</li>--}}
                     <li><a href="{{ route('signup') }}">SIGN UP</a></li>
