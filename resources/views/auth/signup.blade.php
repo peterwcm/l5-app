30f541b7d561ccf90514e08e16727dc3ee740105
Peter Wong <peterwcm@hotmail.com>
2015-09-12 23:15:31 +1000

    foundation standard applied to signup form

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,65 +1,61 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Sign Up</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+<div class="row">
+    <div class="panel">
+        <div>Sign Up</div>
+        <div>
+            @if (count($errors) > 0)
+                <div data-alert class="alert-box alert">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                    <a href="javascript:void(0)" class="close">&times;</a>
+                </div>
+            @endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/signup') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <form role="form" method="POST" action="{{ url('/auth/signup') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
-							</div>
-						</div>
+                <div class="row">
+                    <div class="columns small-12 medium-6">
+                        <label>Email</label>
+                        <input type="email" name="email" value="{{ old('email') }}">
+                    </div>
+                </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+                <div class="row">
+                    <div class="columns small-12 medium-6">
+                        <label>Password</label>
+                        <input type="password" name="password">
+                    </div>
+                    <div class="columns small-12 medium-6">
+                        <label>Confirm Password</label>
+                        <input type="password" name="password_confirmation">
+                    </div>
+                </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+                <div class="row">
+                    <div class="columns small-12 medium-6">
+                        <label>First Name</label>
+                        <input type="text" name="first_name" value="{{ old('first_name') }}">
+                    </div>
+                    <div class="columns small-12 medium-6">
+                        <label>Last Name</label>
+                        <input type="text" name="last_name" value="{{ old('last_name') }}">
+                    </div>
+                </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Sign Up
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+                <div class="row">
+                    <div class="columns small-12 medium-4">
+                        <button type="submit" class="button expand">Sign Up</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 @endsection
